f714097496e8ab134c4a2db17c731e5a09930931
Arun <126227256+ARUN-M-BE@users.noreply.github.com>
2025-11-06 18:51:21 +0530

    update

--- app/how/views.py
+++ app/how/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from django.template import loader
+from .models import Member
+
 def home(request):
     template = loader.get_template('home.html')
     context = {}
@@ -11,3 +13,19 @@ def faq(request):
     template = loader.get_template('faq.html')
     context = {}
     return HttpResponse(template.render(context, request))
+
+def member(request):
+  user = Member.objects.all().values()
+  template = loader.get_template('member.html')
+  context = {
+    'user': user,
+  }
+  return HttpResponse(template.render(context, request))
+
+def details(request, id):
+  user = Member.objects.get(id=id)
+  template = loader.get_template('details.html')
+  context = {
+    'user': user,
+  }
+  return HttpResponse(template.render(context, request))
