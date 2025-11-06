6de2d7857ccbacce3e55728cb41b3c1a29e4bf5e
Arun <126227256+ARUN-M-BE@users.noreply.github.com>
2025-11-06 09:59:47 +0530

    zxxa

---
+++ app/how/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+from django.template import loader
+def home(request):
+    template = loader.get_template('home.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+def faq(request):
+    template = loader.get_template('faq.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
