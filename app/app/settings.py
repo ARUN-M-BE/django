2c2b8bb6c9571dc8ba980091f6eeeb0d3d18aedc
Arun <126227256+ARUN-M-BE@users.noreply.github.com>
2025-11-06 17:27:58 +0530

    ,kjn

--- app/app/settings.py
+++ app/app/settings.py
@@ -77,10 +77,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',     
     }
 }
 
